5462f90a0c8d3dadb70114926d5b8f185f48a0e8
Jeffrey <Jeffrey@simpleyapp.com>
2020-06-20 10:42:58 -0400

    Initial Commit

---
+++ CupCake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CupCake
+//
+//  Created by Jeffrey  on 6/20/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
